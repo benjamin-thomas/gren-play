26fd1846462d7fb5a72660d9532793eacdd31270
Benjamin Thomas <benjamin.guy.thomas@gmail.com>
2025-05-09 21:59:20 +0200

    Evict properly

--- wip.sh
+++ wip.sh
@@ -1,6 +1,7 @@
 (http --print h localhost:4000/ | egrep '^HTTP|Date' && echo "✅ / 1 done") &
 (http localhost:4000/users && echo "✅ /users 2 done") &
 (http --print h localhost:4000/ | egrep '^HTTP|Date' && echo "✅ / 3 done") &
+(http localhost:4000/blog/123 && echo "✅ /blog/123 4 done") &
 
 wait
 echo "✅ All requests completed."
